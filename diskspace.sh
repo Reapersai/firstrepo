eac7aab5d450479fd4f7c6ed84f66b12c5b541d8
Sai-vivek <147052221+Reapersai@users.noreply.github.com>
2025-03-26 13:14:26 +0530

    Create diskspace.sh

---
+++ diskspace.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+  red= tput setaf 1
+  green= tput setaf 2
+  yellow= tput setaf 3
+  reset= tput sqr0
+  space () {
+#here we will give code to get space usage for the server
+
+  }
+  cpu() {
+#here we will give the code to get cpu load of the server
+
+  }
+  mem(){
+#here we will give code to get mem of the server
+
+  }
+#here we will ask for servername
+read -p "Please let me know the servername" x
+#here script should ask for what do we have to do with server
+echo "${yellow}******If you want to check Space give${reset}${red}space${reset}${yellow}**********${reset}"
+echo "${yellow}******If you want to check Memory give${reset}${red}mem${reset}${yellow}**********${reset}"
+echo "${yellow}******If you want to check CPU give${reset}${red}cpu${reset}${yellow}**********${reset}"
+read -p "What do you want to check in server" y
+#here we will call the above mentioned funciones for if loop
+if [ $y == space ]
+then
+space
+elif [ $y == mem ]
+then
+mem
+elif [ $y == cpu ]
+then
+cpu
+fi
+
+  
