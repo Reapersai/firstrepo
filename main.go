3e3e532224d6765dc3271e0b1283e27644949e0a
Reapersai <saivivekvanchanagiri@gmail.com>
2025-08-17 16:44:27 +0530

    git commit -m "updating go app"

--- main.go
+++ main.go
@@ -4,39 +4,41 @@ import "fmt"
 
 func main() {
 	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	var bookings = []string{}
 
 	fmt.Printf("Welcome to %v bookking application!\n", conferenceName)
 	fmt.Printf("Get your tickets here to attend the %v !\n", conferenceName)
 	fmt.Printf("Remember we have only %v Tickets for %v !\n", conferenceTickets, conferenceName)
 	fmt.Printf("We have only %v left!\n\n", remainingTickets)
 
-	var bookings = [50]string{}
+	for {
+		var userName string
+		var lastname string
+		var email string
+		var userTickets uint
+		// Ask user for their name
+		fmt.Printf("Enter your name:- ")
+		fmt.Scan(&userName)
 
-	var userName string
-	var lastname string
-	var email string
-	var userTickets int
-	// Ask user for their name
-	fmt.Printf("Enter your name:- ")
-	fmt.Scan(&userName)
+		// Ask user for their last name
+		fmt.Printf("Enter your last name:- ")
+		fmt.Scan(&lastname)
 
-	// Ask user for their last name
-	fmt.Printf("Enter your last name:- ")
-	fmt.Scan(&lastname)
+		// Ask user for their email
+		fmt.Printf("Enter your email:- ")
+		fmt.Scan(&email)
 
-	// Ask user for their email
-	fmt.Printf("Enter your email:- ")
-	fmt.Scan(&email)
+		// Ask user for number of tickets they want
+		fmt.Printf("How many tickets do you want:- ")
+		fmt.Scan(&userTickets)
+		remainingTickets = remainingTickets - userTickets
+		bookings = append(bookings, userName+" "+lastname)
 
-	// Ask user for number of tickets they want
-	fmt.Printf("How many tickets do you want:- ")
-	fmt.Scan(&userTickets)
-
-	// Print a confirmation message
-	fmt.Printf("Thank you %v for booking %v tickets!\n", userName, userTickets)
-	fmt.Printf("A confirmation email will be sent to %v.\n", email)
-	remainingTickets = remainingTickets - userTickets
-	fmt.Printf("We have %v tickets remaining for %v!\n", remainingTickets, conferenceName)
+		// Print a confirmation message
+		fmt.Printf("Thank you %v for booking %v tickets!\n", userName, userTickets)
+		fmt.Printf("A confirmation email will be sent to %v.\n", email)
+		fmt.Printf("We have %v tickets remaining for %v!\n\n", remainingTickets, conferenceName)
+	}
 }
